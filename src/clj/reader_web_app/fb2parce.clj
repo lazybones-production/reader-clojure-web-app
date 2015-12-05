06a86ad870e0db8ce0ea49905e9b208f1a88e616
Vadim Kravchenko <chaelub@gmail.com>
2015-12-05 19:45:01 +0300

    parser maybe worked

--- src/clj/reader_web_app/fb2parce.clj
+++ src/clj/reader_web_app/fb2parce.clj
@@ -2,6 +2,20 @@
   (:require [clojure.xml :as xml])
   (:gen-class))
 
+; (defn is-en?
+;   [data]
+;     (loop [maps data]
+;       (if (empty? maps)
+;         false
+;         (let [[cmap & maps] maps]
+;          (if (and (= (xml/tag cmap) :lang) (= (xml/content cmap) [:en]))
+;            true
+;            (recur maps))))))
+
+; (defn get-meta
+;   [raw-meta-info, ks]
+;   (map #(get-meta-info raw-meta-info %) {:author}))
+
 (defn get-meta-info
   [raw-meta-info, k]
   (println "\n" raw-meta-info)
@@ -9,7 +23,11 @@
     nil
     (if (map? raw-meta-info)
       (if (= (xml/tag raw-meta-info) k)
-        {k (raw-meta-info :content)}
+        (if (nil? (first (raw-meta-info :content)))
+          nil
+          (if (re-matches #"[A-Za-z0-9\s]*" (first (raw-meta-info :content)))
+            {k (raw-meta-info :content)}
+            nil))
         (get-meta-info (raw-meta-info :content) k))      
       (if (vector? raw-meta-info)
         (loop [nodes raw-meta-info
@@ -25,8 +43,7 @@
   (let [raw-book (xml/parse book-path)
         raw-book-content (xml/content raw-book)
         raw-meta-info (first raw-book-content)]
-    ; (println raw-meta-info)
-    (map #(get-meta-info raw-meta-info %) [:first-name :last-name])))
+    (flatten (map #(get-meta-info raw-meta-info %) [:first-name :last-name :book-title :genre]))))
 
 (defn -main
   [some]
