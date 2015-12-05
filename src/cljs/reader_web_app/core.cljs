82fcc2be297ce68203e99394d0ea29d2e1d382d9
fkuril <fkuril@mail.ru>
2015-12-05 14:40:48 +0300

    addded covers grid

--- src/cljs/reader_web_app/core.cljs
+++ src/cljs/reader_web_app/core.cljs
@@ -12,11 +12,27 @@
   :text "Hello CLJS Application"
   :counter 0
   :contacts []
+  :covers [1 2 3 4 5 6]
   }))
 
 ;; increment counter in state
 (defn incrementCounter [data]
   (om/transact! data :counter inc))
+
+;; Cover
+(defn cover [cover owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:className "cover"} (str cover "!!!")))))
+
+(defn covers-grid [covers owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div #js {:className "covers-grid-container"}
+        (dom/div #js {:className "covers-grid"}
+          (om/build-all cover covers))))))
 
 ;; just plain render, without nested components to prove concepts
 (om/root
@@ -25,14 +41,19 @@
       (render [_]
         (dom/div #js {:className "container"}
           (dom/h1 nil (:text data))
-          (dom/button #js {:className "button" :onClick #(incrementCounter data)} "Click me!")
-          (dom/span nil (:counter data))))))
+           ;; (dom/button #js {:className "button" :onClick #(incrementCounter data)} "Click me!")
+           ;; (dom/span nil (:counter data))
+          (om/build covers-grid (:covers data))))))
   app-state
   {:target (. js/document (getElementById "app"))})
 
 
+
+;; (swap! app-state update-in [:counter] inc)
+
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  ;; (swap! app-state update-in [:counter] inc)
+
 )
