65a73e6e1c3ba8bfda3382d4336ad8cd0096ce70
Seva Zaikov <seva.zaikov@gmail.com>
2015-12-05 04:01:26 +0300

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to reader-clojure-web-app
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/reader_clojure_web_app/core_test.clj
@@ -0,0 +1,7 @@
+(ns reader-clojure-web-app.core-test
+  (:require [clojure.test :refer :all]
+            [reader-clojure-web-app.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
