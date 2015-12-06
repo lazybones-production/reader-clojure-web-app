be0e716388432671b83a124e30f0760a3ce5e825
Vadim Kravchenko <chaelub@gmail.com>
2015-12-06 18:24:25 +0300

    cors added

--- src/clj/reader_web_app/core.clj
+++ src/clj/reader_web_app/core.clj
@@ -5,7 +5,9 @@
             [ring.util.response :refer [response]]
             [ring.middleware.json :as middleware]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
-            [reader-web-app.fb2parce :as fb2]))
+            [ring.middleware.cors :refer [wrap-cors]]
+            [reader-web-app.fb2parce :as fb2]
+            [compojure.handler :as handler]))
 
 (defn get-books [_]
     {:status 200
@@ -13,7 +15,7 @@
 
 (defn create-book [book]
   {:status 200
-   :body {:data (fb2/parse-book (book :book))}})
+   :body {:data (books/create-book file)}})
 
 (defn get-book [id]
   {:status 200
@@ -27,14 +29,47 @@
   (context "/books" []
     (GET "/" [] get-books)
     (GET "/:id" [id] (get-book id))
-    (POST "/" [] create-book)
+    (POST "/" [book] (create-book book))
     (DELETE "/:id" [] delete-book))
   (route/not-found {:body {:error "Not such route"}}))
+
+(defn allow-cross-origin
+  "middleware function to allow crosss origin"
+  [handler]
+  (fn [request]
+   (let [response (handler request)]
+    (assoc-in response [:headers "Access-Control-Allow-Origin"]
+         "*"))))
+
+(defn options-200
+  "middleware function to always 200 an OPTIONS request"
+  [handler]
+  (fn [request]
+    (if (= :options (:request-method request))
+      {:headers {"Access-Control-Allow-Origin" "*"
+                 "Access-Control-Allow-Headers" "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type"
+                 "Access-Control-Allow-Methods" "GET, POST, PUT, OPTIONS"}
+       :body ""
+       :status 204}
+      (handler request))))
 
 (def app
   (-> app-routes
     ;; for automatic parsing requests
     ;; :keywords for getting keys as keywords, not strings
+    (wrap-cors :access-control-allow-origin #".*"
+                      :access-control-allow-credentials "true"
+                      :access-control-allow-headers ["Origin"
+                                                     "X-Requested-With"
+                                                     "Content-Type"
+                                                     "Accept"]
+                      :access-control-allow-methods ["GET"
+                                                     "POST"
+                                                     "PUT"
+                                                     "DELETE"
+                                                     "OPTIONS"])
+    (options-200)
+    (allow-cross-origin)
     (middleware/wrap-json-body {:keywords? true})
     ;; for automatic parsing response
     (middleware/wrap-json-response)
